36446c8fa1b560820294e03c0cf894e0ae3fa5a5
FinCoFella <fincofella@hotmail.com>
2025-09-15 13:47:52 +0100

    Added comments to calc.py file

--- calc.py
+++ calc.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-def unsecured_debt_to_ebitda(df: pd.DataFrame) -> pd.DataFrame:
+##### Annualize the EBIITDA financial metric and calculate the Unsecured-debt-to-EBITDA financial ratio #####
+def unsecured_debt_to_ebitda(df: pd.DataFrame, periods_per_year: int = 4,) -> pd.DataFrame:
 
-    filt = df["Line_Item_Name"].isin(["EBITDA", "Total Unsecured Debt"])
-    piv = (df.loc[filt].pivot(index="Quarter", columns="Line_Item_Name", values="Value").dropna().sort_index())
+    mask = df["Line_Item_Name"].isin(["EBITDA", "Total Unsecured Debt"])
+    pivot = (df.loc[mask].pivot(index="Quarter", columns="Line_Item_Name", values="Value").dropna().sort_index())
 
-    piv["Unsecured_Debt_to_EBITDA"] = piv["Total Unsecured Debt"] / (piv["EBITDA"] * 4)
+    ratio = (pivot["Total Unsecured Debt"] / (pivot["EBITDA"] * periods_per_year)).rename("Unsecured_Debt_to_EBITDA")
 
-    return piv.reset_index()[["Quarter", "Unsecured_Debt_to_EBITDA"]]
+    return ratio.reset_index()
