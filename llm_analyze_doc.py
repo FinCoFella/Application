3fdf3f929035a4740d505d68b0eb869953d2a07f
FinCoFella <fincofella@hotmail.com>
2025-09-15 14:47:25 +0100

    Added comments in llm_analyze_doc file

--- llm_analyze_doc.py
+++ llm_analyze_doc.py
@@ -2,20 +2,24 @@ from typing import Dict
 import fitz
 from openai import OpenAI
 
-def extract_inc_stmt_text(pdf: fitz.Document) -> str:     
-        target_text = ""
+INCOME_STMT_HEADER = "CONDENSED CONSOLIDATED STATEMENT OF OPERATIONS"
 
-        for i, page in enumerate(pdf):
+##### Extracts text from a PDF page containing an income statement #####
+def extract_inc_stmt_text(pdf: fitz.Document) -> str:     
+        page_text = ""
+
+        for page_index, page in enumerate(pdf):
             text = page.get_text()
 
-            if "CONDENSED CONSOLIDATED STATEMENT OF OPERATIONS" in text.upper():
-                target_text = text
-                if i + 1 < len(pdf):
-                    target_text += "\n" + pdf[i + 1].get_text()
+            if INCOME_STMT_HEADER in text.upper():
+                page_text = text
+                if page_index + 1 < len(pdf):
+                    page_text += "\n" + pdf[page_index + 1].get_text()
                 break
 
-        return target_text
+        return page_text
 
+##### LLM prompt instructions to analyze the EBITDA financial metric using data from an income statement #####
 def llm_prompt_for_EBITDA(ticker: str, doc_excerpt: str) -> str:
      return f"""
         The following data is extracted text from {ticker}'s financial filing, which contains the company's income statement for a given quarter in the column "Three Months Ended". 
@@ -24,10 +28,10 @@ def llm_prompt_for_EBITDA(ticker: str, doc_excerpt: str) -> str:
         Note that EBITDA is defined as the sum of net income, interest expense, depreciation and amortization, and provision for income taxes.
         Document Text: {doc_excerpt}"""
 
+##### Invokes the LLM to analyze the EBITDA financial metric in the income statement and returns the analysis #####
 def analyze_quarter_doc(pdf_bytes: bytes, ticker: str, client: OpenAI) -> Dict[str, str]:
      
         doc = fitz.open(stream=pdf_bytes, filetype="pdf")
-
         target_text = extract_inc_stmt_text(doc)
         
         if not target_text:
